67090f164751181dd11b1f612d7ca979753da559
Kathy Sweeney <kat@katdidit.com>
2022-10-22 11:43:36 -0600

    Learning git hut. hubba hubba

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+	echo 'Welcome to our website, Please Log In' ;
+	echo '<form action=post><input type="text" name="login" value="" />';
+	echo '<input type="text" name="password" value="" />'
+?>
